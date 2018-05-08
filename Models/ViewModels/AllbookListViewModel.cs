648dd11068ee0faf539e67e3fa477691885bea05
Sigrún Tinna Sigurbjarnadóttir <sigruntinnasigurbjarnadottir@Sigruns-MacBook-Air.local>
2018-05-08 11:33:34 +0000

    view models

---
+++ Models/ViewModels/AllbookListViewModel.cs
@@ -0,0 +1,11 @@
+namespace TheBookCave.Models.ViewModels
+{
+    public class AllbookListViewModel
+    {
+        public int bookId {get; set;}
+        public string titel {get; set;}
+        public int pages {get; set;}
+        public int price {get ; set;} 
+    }
+}
+    
---
+++ Models/ViewModels/EbookListViewModel.cs
@@ -0,0 +1,14 @@
+namespace TheBookCave.Models.ViewModels
+{
+    public class EbookListViewModel
+    {
+        public int bookId {get; set;}
+        public string authorId {get; set;}
+        public string translator {get; set;}
+        public string originalLanguage {get; set;}
+        public string languages {get; set;}
+        public int pages {get; set;}
+        public int price {get ; set;} 
+    }
+}
+    
