18875a7e3f71cebc997a98c6549244c4c7adc615
Sigrún Tinna Sigurbjarnadóttir <sigruntinnasigurbjarnadottir@Sigruns-MacBook-Air.local>
2018-05-08 16:26:53 +0000

    controler

---
+++ Controllers/BookController.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.Models.EntityModels;
+
+namespace TheBookCave.Controllers
+{
+    public class BookController: Controller
+    {
+        
