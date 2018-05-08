9f2a969fa5ef829365014ff3faa2343586d87bd6
Kolfinna Jónsdóttir <kolfinna17@ru.is>
2018-05-08 12:36:58 +0000

    start of user, fixed book database

---
+++ Migrations/20180508122613_initialMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TheBookCave.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    age = table.Column<int>(nullable: false),
+                    alive = table.Column<bool>(nullable: false),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    author = table.Column<string>(nullable: true),
+                    authorId = table.Column<int>(nullable: false),
+                    genre = table.Column<string>(nullable: true),
+                    language = table.Column<string>(nullable: true),
+                    originalLanguage = table.Column<string>(nullable: true),
+                    pages = table.Column<int>(nullable: false),
+                    price = table.Column<int>(nullable: false),
+                    publisher = table.Column<string>(nullable: true),
+                    releaseYear = table.Column<int>(nullable: false),
+                    title = table.Column<string>(nullable: true),
+                    translator = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- Models/ViewModels/AllbookListViewModel.cs
+++ Models/ViewModels/AllbookListViewModel.cs
@@ -3,7 +3,7 @@ namespace TheBookCave.Models.ViewModels
     public class AllbookListViewModel
     {
         public int bookId {get; set;}
-        public string titel {get; set;}
+        public string title {get; set;}
         public int pages {get; set;}
         public int price {get ; set;} 
     }
