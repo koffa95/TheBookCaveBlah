d3b59d9b1c53b7ff315f67b8b3b9a0bd2a8945aa
Kolfinna Jónsdóttir <kolfinna17@ru.is>
2018-05-08 16:42:47 +0000

    bookController

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +9,3 @@ namespace TheBookCave.Controllers
     {
     }
 }    
-=======
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using MovieApp.Models.EntityModels;
-
-namespace TheBookCave.Controllers
-{
-    public class BookController: Controller
-    {
-        
->>>>>>> 38cb1104b38405131faedaced74830f821f8526d
