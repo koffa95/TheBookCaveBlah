966be3ad336c379e366be75b9927a2ebe4c4d629
Sigrún Tinna Sigurbjarnadóttir <sigruntinnasigurbjarnadottir@Sigruns-MacBook-Air.local>
2018-05-10 02:41:57 +0000

    details

--- Models/ViewModels/BestsellerListViewModel.cs
+++ Models/ViewModels/BestsellerListViewModel.cs
@@ -7,7 +7,7 @@ namespace TheBookCave.Models.ViewModels
         public int authorId {get; set;}
         public string translator {get; set;}
         public string originalLanguage {get; set;}
-        public string languages {get; set;}
+        public string language {get; set;}
         public int pages {get; set;}
         public int price {get; set;}
 
