06384c97516bdbe2676e470aabf8102ffa4b8c1b
Kolfinna Jónsdóttir <kolfinna17@ru.is>
2018-05-10 02:20:14 +0000

    header nav bar

---
+++ Models/ViewModels/Top10ViewModel.cs
@@ -0,0 +1,15 @@
+namespace TheBookCave.Models.ViewModels
+{
+    public class Top10ListViewModel
+    {
+        public int bookId {get; set;}
+        public string authorId {get; set;}
+        public string title {get; set;}
+        public string translator {get; set;}
+        public string originalLanguage {get; set;}
+        public string languages {get; set;}
+        public int pages {get; set;}
+        public int price {get ; set;} 
+    }
+}
+    
